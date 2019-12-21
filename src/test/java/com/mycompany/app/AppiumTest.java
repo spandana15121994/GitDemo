381dd7bbcfccc9194cf9ca6e3909d0dae2646695
Dell <spandana7244@gmail.com>
2019-12-21 20:44:24 +0530

    changes done to the code after downloading it into E driver workspace

--- src/test/java/com/mycompany/app/AppiumTest.java
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -8,10 +8,14 @@ public class AppiumTest {
 	public void NativeAtomation()
 	{
 		System.out.println("NativeAtomation");
+		System.out.println("changes done 1");
+		System.out.println("changes done 4");
 	}
 	@Test
 	public void IOSApps()
 	{
 		System.out.println("IOSApps");
+		System.out.println("chnages done 2");
+		System.out.println("changes done 3");
 	}
 }
