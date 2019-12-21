dc35c0ae79b2ff3f5006e58157bb397a568a78ec
Dell <spandana7244@gmail.com>
2019-12-22 01:27:49 +0530

    sunny method

--- src/test/java/com/mycompany/app/SeleniumTest.java
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -18,11 +18,17 @@ public class SeleniumTest {
 
 		System.out.println("elementUI");
 		System.out.println("updated selenium test 2");
+		
 		System.out.println("updates done by Asian developer as a branch");
 	}
 
 	@Test
 	public void AmericanMethod()
+	{
+		System.out.println("Added method by American developer via branch");
+	}
+	@Test
+	public void SunnyMethod()
 	{
 		System.out.println("Added method by American developer via branch");
 	}
